29eab9a210555fb2dba463600fd5bf7de0a4acbd
Kuldeep <lagade.kuldeep2503@gmail.com>
2024-08-14 16:22:16 +0530

    added the new code

--- func-testinggithubaction/__init__.py
+++ func-testinggithubaction/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 from fastapi import FastAPI
 from starlette.middleware.wsgi import WSGIMiddleware
 
-# Add the app directory to sys.path
+# Add the app directory to the system path
 app_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "app"))
 sys.path.insert(0, app_dir)
 
