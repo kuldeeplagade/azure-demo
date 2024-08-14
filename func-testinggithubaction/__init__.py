e17f64d0b5777ea82cc6b5971b422fafa7eb1ee7
Kuldeep <lagade.kuldeep2503@gmail.com>
2024-08-14 09:51:08 +0530

    added the new chnages one

--- func-testinggithubaction/__init__.py
+++ func-testinggithubaction/__init__.py
@@ -1,13 +1,46 @@
 import logging
-import azure.functions as func
-from mangum import Mangum
-from app.main import app  # Import your FastAPI app
+from azure.functions import HttpRequest, HttpResponse
+from app.main import app
+from fastapi import Request
+from starlette.responses import Response
+from starlette.types import Scope, Receive, Send
 
-# Wrap the FastAPI app with Mangum to handle requests in a serverless environment
-handler = Mangum(app)
+class AsgiToAzureFunction:
+    def __init__(self, app):
+        self.app = app
 
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    async def __call__(self, req: HttpRequest, context) -> HttpResponse:
+        asgi_req = self._convert_to_asgi(req)
+        response = Response()
+        await self.app(asgi_req, asgi_req.receive, response.send)
+        return self._convert_to_azure_response(response)
 
-    # Use the Mangum handler to process the incoming HTTP request
-    return func.AsgiFunctionHandler(handler).handle(req, context)
+    def _convert_to_asgi(self, req: HttpRequest) -> Request:
+        scope = {
+            "type": "http",
+            "method": req.method,
+            "path": req.route_params.get("route", "/"),
+            "query_string": req.url.query.encode(),
+            "headers": [(k.encode(), v.encode()) for k, v in req.headers.items()],
+        }
+        return Request(scope, receive=self._receive(req))
+
+    def _receive(self, req: HttpRequest):
+        async def receive() -> Receive:
+            return {"type": "http.request", "body": req.get_body(), "more_body": False}
+
+        return receive
+
+    def _convert_to_azure_response(self, response: Response) -> HttpResponse:
+        return HttpResponse(
+            body=response.body,
+            status_code=response.status_code,
+            headers=dict(response.headers),
+            mimetype=response.media_type,
+        )
+
+handler = AsgiToAzureFunction(app)
+
+def main(req: HttpRequest, context) -> HttpResponse:
+    logging.info('Processing request with FastAPI in Azure Functions.')
+    return handler(req, context)
